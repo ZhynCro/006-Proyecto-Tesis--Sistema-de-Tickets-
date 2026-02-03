a5e7285bab2e40b6cf1db8e62a2fa65d978aac16
Cesar Boada <cesarboada3c@gmail.com>
2026-02-02 22:39:47 -0400

    Crear modelo de usuarios

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import usuario, departamento, sede
 
 # Register your models here.
+
+class usuarioAdmin(UserAdmin):
+    list_display = ('ID_empleado', 'username', 'email', 'sede', 'departamento', 'is_staff', 'is_active')
+    ordering = ('ID_empleado',)
+    search_fields = ('ID_empleado', 'username', 'email')
+
+    fieldsets = UserAdmin.fieldsets + (
+        ('Información Corporativa', {
+            'fields': ('ID_empleado', 'sede', 'departamento')
+        }),
+    )
+
+    # 3. Configuración para CREAR un usuario nuevo
+    # Esto asegura que aparezcan los campos en el formulario inicial
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Información Corporativa', {
+            'fields': ('ID_empleado', 'sede', 'departamento')
+        }),
+    )
+
+admin.site.register(usuario, usuarioAdmin)
+admin.site.register(departamento)
+admin.site.register(sede)
--- users/models.py
+++ users/models.py
@@ -1,6 +1,59 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 
 # Create your models here.
 class usuario(AbstractUser):
-    pass
+    validador_empleado = RegexValidator(
+        regex=r'^\d{4}',
+        message="el numero de empleado debe tener 4 digitos"
+    )
+
+    ID_empleado = models.CharField(
+        max_length=4,
+        unique=True,
+        validators=[validador_empleado],
+        help_text="ID de empleado de 4 digitos",
+        default="0000"
+    )
+
+    USERNAME_FIELD = 'ID_empleado'
+
+    departamento = models.ForeignKey(
+        'departamento',
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='usuarios'
+        )
+    
+    sede = models.ForeignKey(
+        'sede',
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='usuarios'
+        )
+
+    REQUIRED_FIELDS = ['username', 'email']
+    def __str__(self):
+        return f"{self.ID_empleado} - {self.get_full_name()}"
+    
+class departamento(models.Model):
+    nombre = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.nombre
+    
+    class Meta:
+        verbose_name_plural = "departamentos"
+    
+class sede(models.Model):
+    nombre = models.CharField(max_length=50, unique=True)
+
+    departamentos = models.ManyToManyField(
+        departamento,
+        related_name='sedes',
+        help_text="Departamentos en esta sede"
+        )
+
+    def __str__(self):
+        return self.nombre
